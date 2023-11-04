ad5c20d218e52c35cafe0f73bb4ff21b257328dd
ricesush <pixelartme@gmail.com>
2023-11-04 16:37:22 +0800

    added pictures and images

--- src/app/Components/navmenu.jsx
+++ src/app/Components/navmenu.jsx
@@ -10,35 +10,49 @@ export default function Navmenu() {
             <div className="container-fluid">
                 <Link className="navbar-brand fw-bold fs-6" href='/'>e<span className='text-orange'>-</span>jesan<span className='text-orange'>.</span></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon "></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link fs-6 active" aria-current="page" href='/'>Home</Link>
+                    <ul className="d-lg-flex d-none navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li className="nav-item border-end-orange">
+                            <Link className="nav-link fs-6 fw-bold active" aria-current="page" href='/'>Home</Link>
+                        </li>
+                        <li className="nav-item border-end-orange">
+                            <Link className="nav-link fs-6 fw-bold" href='/projects'>Projects</Link>
+                        </li>
+                        <li className="nav-item border-end-orange">
+                            <Link className="nav-link fs-6 fw-bold" href='/about'>About Me</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link fs-6" href='/projects'>Projects</Link>
+                            <Link className="nav-link fs-6 fw-bold" href='/contact'>Contact</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link fs-6" href='/about'>About Me</Link>
+                    </ul>
+                    <ul className="d-lg-none navbar-nav mx-auto mb-2 mb-lg-0 text-center">
+                        <li className="nav-item border-bottom border-opacity-25 border-secondary">
+                            <Link className="nav-link fs-6 fw-bold active" aria-current="page" href='/'>Home</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link fs-6" href='/contact'>Contact</Link>
+                        <li className="nav-item border-bottom border-opacity-25 border-secondary">
+                            <Link className="nav-link fs-6 fw-bold" href='/projects'>Projects</Link>
+                        </li>
+                        <li className="nav-item border-bottom border-opacity-25 border-secondary">
+                            <Link className="nav-link fs-6 fw-bold" href='/about'>About Me</Link>
+                        </li>
+                        <li className="nav-item border-bottom border-opacity-25 border-secondary">
+                            <Link className="nav-link fs-6 fw-bold" href='/contact'>Contact</Link>
+                        </li>
+                    </ul>
+                    <ul className="list-group list-group-horizontal d-flex align-items-center">
+                        <li className="list-group-item bg-transparent border-0 ms-lg-0 ms-auto">
+                            <Link className="nav-link active" aria-current="page" href='/'><i className="fs-5 bi bi-github"></i></Link>
+                        </li>
+                        <li className="list-group-item bg-transparent border-0">
+                            <Link className="nav-link" href='/'><i className="fs-5 bi bi-linkedin"></i></Link>
+                        </li>
+                        <li className="list-group-item  bg-transparent border-0 me-lg-0 me-auto">
+                            <Link className="nav-link" href='/'><i className="fs-5 bi bi-facebook"></i></Link>
                         </li>
                     </ul>
                 </div>
-                <ul className="list-group list-group-horizontal">
-                    <li className="list-group-item  bg-transparent border-0">
-                        <Link className="nav-link active" aria-current="page" href='/'><i className="fs-5 bi bi-github"></i></Link>
-                    </li>
-                    <li className="list-group-item  bg-transparent border-0">
-                        <Link className="nav-link" href='/'><i className="fs-5 bi bi-linkedin"></i></Link>
-                    </li>
-                    <li className="list-group-item  bg-transparent border-0">
-                        <Link className="nav-link" href='/'><i className="fs-5 bi bi-facebook"></i></Link>
-                    </li>
-                </ul>
             </div>
         </nav>
     </section>
--- src/app/Components/sideBarMenu.jsx
+++ src/app/Components/sideBarMenu.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
+import ProfilePic from '../../img/profilePic.png'
 // import ProfilePicuter
 
 export default function SideBarMenu() {
   return (
-    <section className='border rounded my-3'>
-        <div className="card bg-dark border-dark" >
-          <div className="card-header">Header</div>
+    <section className='rounded shadow my-3'>
+        <div className="card bg-white " >
+          <div className="card-header text-center shadow-sm">
+            <Image
+              src={ProfilePic}
+              alt='EJ Esan Profile Picture'
+              width={100}
+              className='m-4'
+            ></Image>
+            <p className='fw-bold fs-6 p-0 m-0  text-dark'>Erick-john Santosidad</p>
+            <p className='fs-6 fw-bold p-0 m-0 border-top border-secondary border-opacity-50  text-secondary'><i className="bi bi-laptop text-dark"></i> Full Stack Web Developer</p>
+          </div>
           <div className="card-body">
-            <h5 className="card-title">Dark card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="card-text text-dark fs-6"><i class="bi bi-geo-alt-fill text-orange"></i> Quezon City, Philippines</p>
+            <p className="card-text text-dark fs-6"><i class="bi bi-translate text-orange"></i> English, Filipino</p>
           </div>
         </div>
     </section>
