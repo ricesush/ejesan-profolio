cad193a1e211cc2f942405e16c6f128767abb08e
ricesush <pixelartme@gmail.com>
2023-11-12 04:20:15 +0800

    added active page animation

--- src/app/Components/navmenu.jsx
+++ src/app/Components/navmenu.jsx
@@ -1,56 +1,64 @@
-'user client'
+'use client'
 
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function Navmenu() {
+    const pathName = usePathname();
+    const navLink = [
+        {
+            pageName: "Home",
+            pageLink: "/",
+        },
+        {
+            pageName: "Projects",
+            pageLink: "/projects",
+        },
+        {
+            pageName: "Contact",
+            pageLink: "/contact",
+        }
+    ];
+
+    const socmedLinks = [
+        {
+            icon: "bi-github",
+            link: "https://github.com/ricesush",
+        },
+        {
+            icon: "bi-linkedin",
+            link: "https://www.linkedin.com/in/ejsan/",
+        },
+        {
+            icon: "bi-facebook",
+            link: "https://www.facebook.com/RiceSush",
+        }
+    ];
+
+
   return (
     <section className='shadow-sm bg-dark-gray'>
-        <nav className="navbar navbar-expand-lg container">
+        <nav className="navbar navbar-focus navbar-dark  navbar-expand-lg container">
             <div className="container-fluid">
                 <Link className="navbar-brand fw-bold fs-6" href='/'>e<span className='text-orange'>-</span>jesan<span className='text-orange'>.</span></Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler navbar-focus" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon "></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    {/* <ul className="d-lg-flex d-none navbar-nav mx-auto mb-2 mb-lg-0">
-                        <li className="nav-item border-end-orange">
-                            <Link className="nav-link fs-6 fw-bold active" aria-current="page" href='/'>Home</Link>
-                        </li>
-                        <li className="nav-item border-end-orange">
-                            <Link className="nav-link fs-6 fw-bold" href='/projects'>Projects</Link>
-                        </li>
-                        <li className="nav-item border-end-orange">
-                            <Link className="nav-link fs-6 fw-bold" href='/about'>About Me</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link fs-6 fw-bold" href='/contact'>Contact</Link>
-                        </li>
-                    </ul> */}
                     <ul className="d-lg-none navbar-nav mx-auto mb-2 mb-lg-0 text-center">
-                        <li className="nav-item border-bottom border-opacity-25 border-secondary">
-                            <Link className="nav-link fs-6 fw-bold active" aria-current="page" href='/'>Home</Link>
-                        </li>
-                        <li className="nav-item border-bottom border-opacity-25 border-secondary">
-                            <Link className="nav-link fs-6 fw-bold" href='/projects'>Projects</Link>
-                        </li>
-                        <li className="nav-item border-bottom border-opacity-25 border-secondary">
-                            <Link className="nav-link fs-6 fw-bold" href='/about'>About Me</Link>
-                        </li>
-                        <li className="nav-item border-bottom border-opacity-25 border-secondary">
-                            <Link className="nav-link fs-6 fw-bold" href='/contact'>Contact</Link>
-                        </li>
+                        {navLink.map(({pageLink, pageName}) => (
+                            <li key={pageName} className="nav-item border-bottom border-opacity-25 border-secondary">
+                                <Link  className={`nav-link fs-6 fw-bold ${pathName == pageLink ? "activePage" : ""}`} aria-current="page" href={pageLink}>{pageName}</Link>
+                            </li>
+                        ))}
                     </ul>
-                    <ul className="list-group list-group-horizontal d-flex align-items-center ms-auto">
-                        <li className="list-group-item bg-transparent border-0 ms-lg-0 ms-auto">
-                            <Link className="nav-link active" aria-current="page" href='/'><i className="fs-5 bi bi-github"></i></Link>
-                        </li>
-                        <li className="list-group-item bg-transparent border-0">
-                            <Link className="nav-link" href='/'><i className="fs-5 bi bi-linkedin"></i></Link>
-                        </li>
-                        <li className="list-group-item  bg-transparent border-0 me-lg-0 me-auto">
-                            <Link className="nav-link" href='/'><i className="fs-5 bi bi-facebook"></i></Link>
-                        </li>
+                    <ul className="list-group list-group-horizontal d-flex align-items-center ms-auto me-lg-0 me-auto col-lg col-3">
+                        {socmedLinks.map(({icon, link}) => (
+                            <li key={icon} className="list-group-item bg-transparent border-0 ms-lg-0 ms-auto">
+                                <Link  className="nav-link" target='_blank' href={link}><i className={`fs-5 bi ${icon}`}></i></Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
--- src/app/Components/sideBarMenu.jsx
+++ src/app/Components/sideBarMenu.jsx
@@ -1,10 +1,48 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import ProfilePic from '../../img/profilePic.png'
+import { usePathname } from 'next/navigation';
 // import ProfilePicuter
 
 export default function SideBarMenu() {
+    const pathName = usePathname();
+    const navLink = [
+        {
+            pageName: "Home",
+            pageLink: "/",
+        },
+        {
+            pageName: "Projects",
+            pageLink: "/projects",
+        },
+        {
+            pageName: "Contact",
+            pageLink: "/contact",
+        }
+    ];
+
+    const contactDetails = [
+        {
+            details: "Quezon City, Philippines",
+            icon: "bi-geo-alt-fill",
+        },
+        {
+            details: "English, Filipino",
+            icon: "bi-translate",
+        },
+        {
+            details: "Pixelartme@gmail.com",
+            icon: "bi-envelope-at-fill",
+        },
+        {
+          details: "+63999-551-4413",
+          icon: "bi-telephone-fill",
+        }
+    ];
+
   return (
     <section className='shadow'>
         <div className="card rounded-0 border-0" >
@@ -20,23 +58,16 @@ export default function SideBarMenu() {
                 <div className=''>
                   <p className='fw-bold fs-4 p-0 m-0 text-dark'>Erick-john Santosidad</p>
                   <p className='fs-6 fw-bold p-0 m-0 border-top border-secondary border-opacity-50  text-secondary'><i className="bi bi-laptop text-dark"></i> Full Stack Web Developer</p>
-                  <button className='btn btn-orange rounded-5 btn-sm mt-2 fw-medium d-flex align-items-center py-0'>Download Resume <i className="bi bi-cloud-arrow-down ms-2 fs-5 bg-transparent"></i></button>
+                  <button className='btn btn-orange shadow-sm rounded-5 btn-sm mt-2 fw-medium d-flex align-items-center py-0'>Download Resume <i className="bi bi-cloud-arrow-down ms-2 fs-5 bg-transparent"></i></button>
                 </div>
               </div>
               <nav className="container">
                   <ul className="list-group list-group-horizontal d-flex align-items-center border-0">
-                      <li className="list-group-item bg-transparent border-0 nav-item text-center">
-                        <Link className="nav-link fs-6 fw-bold text-secondary px-2 border-bot-hover active" aria-current="page" href='/'>Home</Link>
-                      </li>
-                      <li className="list-group-item bg-transparent border-0 nav-item text-center">
-                        <Link className="nav-link fs-6 fw-bold text-secondary px-2 border-bot-hover" href='/projects'>Projects</Link>
-                      </li>
-                      <li className="list-group-item bg-transparent border-0 nav-item text-center">
-                        <Link className="nav-link fs-6 fw-bold text-secondary px-2 border-bot-hover text-nowrap" href='/about'>About Me</Link>
-                      </li>
-                      <li className="list-group-item bg-transparent border-0 nav-item text-center">
-                        <Link className="nav-link fs-6 fw-bold text-secondary px-2 border-bot-hover" href='/contact'>Contact</Link>
-                      </li>
+                      {navLink.map(({pageName, pageLink}) => (
+                          <li key={pageName} className="list-group-item bg-transparent border-0 nav-item text-center">
+                            <Link className={`nav-link fs-6 fw-bold text-secondary px-2 border-bot-hover ${pathName == pageLink ? "activePage" : ""}`} aria-current="page" href={pageLink}>{pageName}</Link>
+                          </li>
+                      ))}
                   </ul>
               </nav>
             </section>
@@ -44,22 +75,12 @@ export default function SideBarMenu() {
             <section className='col-3 ms-auto text-dark border-start ps-4 d-md-block d-none'>
               <table className="table table-borderless fs-6">
                 <tbody>
-                  <tr>
-                    <td className='col-1 bg-transparent'><i className="bi bi-geo-alt-fill text-orange"></i></td>
-                    <td className='text-dark  bg-transparent'>Quezon City, Philippines</td>
-                  </tr>
-                  <tr>
-                    <td className='col-1 bg-transparent'><i className="bi bi-translate text-orange"></i></td>
-                    <td className='text-dark bg-transparent'>English, Filipino</td>
-                  </tr>
-                  <tr>
-                    <td className='col-1 bg-transparent'><i className="bi bi-envelope-at-fill text-orange"></i></td>
-                    <td className='text-dark bg-transparent'>Pixelartme@gmail.com</td>
-                  </tr>
-                  <tr>
-                    <td className='col-1 bg-transparent'><i className="bi bi-telephone-fill text-orange"></i></td>
-                    <td className='text-dark bg-transparent'>+63999-551-4413</td>
-                  </tr>
+                  {contactDetails.map(({icon, details}) => (
+                      <tr key={icon}>
+                        <td className='col-1 bg-transparent'><i className={`bi ${icon} text-orange`}></i></td>
+                        <td className='text-dark  bg-transparent'>{details}</td>
+                      </tr>
+                  ))}
                 </tbody>
               </table>
             </section>
